4d02e221b26cc4ad83c9de97165a5ab4e91d874a
Albert <me@albert.sh>
2018-09-10 22:36:17 +0200

    Fix bug which caused a settings reset on each extension update

--- extension/bg.js
+++ extension/bg.js
@@ -3,5 +3,18 @@ chrome.browserAction.onClicked.addListener(function (tab) { //Fired when the use
       //chrome.tabs.create({ url: "app/index.html" });
 });
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.local.set({interleavingTrigger: 2, lastDeck:"", interleavingDisabled:false, betaEnabled:false, deckNames:{}, excludedDecks:[]}, function() {}); //Set default values
+	let defaults={
+		interleavingTrigger: 2, 
+		lastDeck:"", 
+		interleavingDisabled:false, 
+		betaEnabled:false, 
+		deckNames:{}, 
+		excludedDecks:[]
+	};
+	chrome.storage.local.get(Object.keys(defaults), function(result) {
+		for(let s in result){
+			delete defaults[s];
+		}
+		chrome.storage.local.set(defaults, function() {}); //Set missing default values
+	});
 });
