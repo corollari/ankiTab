ce2c3a97891ebdf8026e5fb1acaab5d9573564a7
Albert <me@albert.sh>
2018-09-25 00:14:19 +0200

    Add error notification Triggered when ankiTab can't connect to anki

--- extension/app/js/extended/rotateDeck.js
+++ extension/app/js/extended/rotateDeck.js
@@ -16,13 +16,17 @@ export default function rotateDeck(DOMchange=true){
 				let decksLeft=result.deckNames.filter((deck)=>result.excludedDecks.indexOf(deck)==-1 && emptyDeckNames.indexOf(deck)==-1); 
 				if(!decksLeft.length){ //All cards scheduled for today have been reviewed
 					if(DOMchange){
-						$("body").html("<div style='text-align: center;' class='vertical-center'><main class='container'><h1>Congratulations!</h1><hr><h3>All cards scheduled for today have been reviewed</h3></main></div>");
+						document.body.innerHTML="<div style='text-align: center;' class='vertical-center'><main class='container'><h1>Congratulations!</h1><hr><h3>All cards scheduled for today have been reviewed</h3></main></div>";
 					}
 				}
 				else{
 					let lastDeck=decksLeft.sort().filter((d)=>d>result.lastDeck).shift() || decksLeft[0];
 					ankiConnectInvoke("guiDeckReview", {name: lastDeck})
-						.then(()=>chrome.storage.local.set({lastDeck: lastDeck}, resolve));
+						.then(()=>chrome.storage.local.set({lastDeck: lastDeck}, resolve))
+						.catch((e)=>{
+							document.querySelector("#flashcard").innerHTML='<div class="alert alert-danger" role="alert">Make sure that anki is running and <a href="https://ankiweb.net/shared/info/2055492159" class="alert-link">ankiConnect</a> is installed.</div>';
+							window.location.href="chrome-search://local-ntp/local-ntp.html"
+						});
 				}
 				let now=new Date();
 				result.emptyDecks.filter((deck)=>Math.abs(now-deck.date)<checkEmptyDelay);
