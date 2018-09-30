e9710e12662853e3f75168e20a54cfdd1109040d
Albert <me@albert.sh>
2018-09-30 16:22:48 +0200

    Add link to options in all new tabs

--- extension/app/js/main.js
+++ extension/app/js/main.js
@@ -1,3 +1,4 @@
 import startStudy from "./cards.js";
 document.querySelector("base").href="/";
 startStudy();
+document.querySelector("#optionsBtn>a").href=chrome.runtime.getURL("options/index.html");
