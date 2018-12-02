b6699abf47c3bb79ec9841574b8e015b81299325
Albert <me@albert.sh>
2018-12-03 00:04:48 +0100

    Fix #9

--- extension/app/js/main.js
+++ extension/app/js/main.js
@@ -1,4 +1,8 @@
 import startStudy from "./cards.js";
+import ankiConnectInvoke from "../../../libs/ankiConnect.js"
 document.querySelector("base").href="/";
 startStudy();
 document.querySelector("#optionsBtn>a").href=chrome.runtime.getURL("options/index.html");
+window.onbeforeunload=()=>{
+	ankiConnectInvoke("guiDeckBrowser")
+};
