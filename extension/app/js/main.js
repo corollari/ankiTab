e965ef41dd25f94f79d4974e1e078626c6d3880e
Albert <github@albert.sh>
2019-05-25 22:01:24 +0200

    Implement #20

--- extension/app/js/main.js
+++ extension/app/js/main.js
@@ -3,6 +3,7 @@ import ankiConnectInvoke from "../../../libs/ankiConnect.js"
 document.querySelector("base").href="/";
 startStudy();
 document.querySelector("#optionsBtn>a").href=chrome.runtime.getURL("options/index.html");
+document.querySelector("#optionsBtn>#syncBtn").addEventListener("click", ()=>ankiConnectInvoke("sync"));
 window.onbeforeunload=()=>{
 	ankiConnectInvoke("guiDeckBrowser")
 };
