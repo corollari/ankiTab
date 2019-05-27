a18b914255f3b2f09c9a7dae35f9290231d646d9
throwaway020863029 <throwaway020863029@fastmail.com>
2019-05-26 20:19:13 -0700

    Re-write entire sandbox document to force JS to be run

--- extension/app/js/sandbox.js
+++ extension/app/js/sandbox.js
@@ -3,6 +3,11 @@ window.addEventListener('message', function(event) {
     case 'render':
         var html = event.data.html;
         document.body.querySelector(".card").innerHTML = html;
+        // Reload the entire document so that script tags are run.
+        var outerHTML = document.documentElement.outerHTML;
+        document.open();
+        document.write(outerHTML);
+        document.close();
         break;
     }
 })
